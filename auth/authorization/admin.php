b8c3687a117026fb24156cc1515c2c2047da2620
messabih-khalil <msbih.khalil@gmail.com>
2023-04-01 13:00:23 -0700

    admin

--- auth/authorization/admin.php
+++ auth/authorization/admin.php
@@ -3,6 +3,6 @@
     
     // Check if user is logged in
     if (!(isset($_SESSION["user_id"]) && $_SESSION["role"] === 'admin')) {
-        header('Location:/places_app/app/home.php');
+        header('Location:/places_app/app/index.php');
     }
 ?>
